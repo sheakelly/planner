c617bfa964aee1c913bb3ac517d3f889b080cf0e
Shea Kelly <shea.kelly@lwb.org.au>
2026-01-02 14:02:09 +1100

    Fix eslint config to ignore build output and config files

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,15 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    ignores: [
+      '.output/**',
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      '*.config.js',
+    ],
+  },
+]
